fab4484de16f2a3a1058d9266a5669f3eb10a6ef
pranavv481 <pranavv481@gmail.com>
2020-05-26 15:29:50 +0530

    intervalHook

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import IntervalClassCounter from './components/IntervalClassCounter';
+import IntervalHookCounter from './components/IntervalHookCounter';
+
+
+
+
+
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+        <IntervalClassCounter />
+        <IntervalHookCounter />
+      </div>
+    );
+  }
+}
+
+export default App;
